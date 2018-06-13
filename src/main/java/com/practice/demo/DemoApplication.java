a291eb11b4c74707554d84d9f86bb94de0c8fe72
Haimanti Saha <anik.saha.4096@gmail.com>
2018-06-12 22:20:40 -0400

    get all the bean names of the project

---
+++ src/main/java/com/practice/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.practice.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+		
+		String[] beanNames =  ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for(String name: beanNames) {
+			System.out.println(name);
+		}
+	}
+}
