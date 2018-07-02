88cd6ab02ea3bdb367b1c21444eab1cdaba9f7ea
Haimanti Saha <anik.saha.4096@gmail.com>
2018-07-02 17:17:08 -0400

    use of path variable and bean

--- src/main/java/com/practice/demo/controller/HomeController.java
+++ src/main/java/com/practice/demo/controller/HomeController.java
@@ -2,7 +2,10 @@ package com.practice.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.practice.demo.models.HelloWorldBean;
 
 @RestController
 public class HomeController {
@@ -17,4 +20,18 @@ public class HomeController {
    public String home() {
 	   return msg;
    }
+   
+   @GetMapping("/hello-world")	
+   public HelloWorldBean helloWorld() {
+	   return new HelloWorldBean("Hello World");
+   }
+   
+   @GetMapping("/hello-world/path-variabe/{name}")	
+   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+	   return new HelloWorldBean(String.format("Hello World %s" , name));
+   }
+   
+   
+   
+   
 }
