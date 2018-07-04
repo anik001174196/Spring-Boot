4df4b73598d48b682c47f59690020d84c2a7d342
Haimanti Saha <anik.saha.4096@gmail.com>
2018-07-03 22:24:07 -0400

    internationalizaiton part1

--- src/main/java/com/practice/demo/DemoApplication.java
+++ src/main/java/com/practice/demo/DemoApplication.java
@@ -1,13 +1,16 @@
 package com.practice.demo;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
-
-import com.practice.demo.models.MyConfigModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -15,13 +18,25 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-		
-		String[] beanNames =  ctx.getBeanDefinitionNames();
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
 		Arrays.sort(beanNames);
-		for(String name: beanNames) {
-			System.out.println(name);
-		}
-		
-		
+
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messgaeSource = new ResourceBundleMessageSource();
+		// setBaseName should be equal to i18n base file message
+		messgaeSource.setBasename("messages");
+		return messgaeSource;
+
 	}
 }
--- src/main/java/com/practice/demo/controller/HomeController.java
+++ src/main/java/com/practice/demo/controller/HomeController.java
@@ -1,37 +1,49 @@
 package com.practice.demo.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.demo.models.HelloWorldBean;
 
 @RestController
 public class HomeController {
-	
-   @Value("${spring.profiles.active}")
-   private String environment;
-   
-   @Value("${msg}")
-   private String msg;
-   
-   @GetMapping("/")	
-   public String home() {
-	   return msg;
-   }
-   
-   @GetMapping("/hello-world")	
-   public HelloWorldBean helloWorld() {
-	   return new HelloWorldBean("Hello World");
-   }
-   
-   @GetMapping("/hello-world/path-variabe/{name}")	
-   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-	   return new HelloWorldBean(String.format("Hello World %s" , name));
-   }
-   
-   
-   
-   
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@Value("${spring.profiles.active}")
+	private String environment;
+
+	@Value("${msg}")
+	private String msg;
+
+	@GetMapping("/")
+	public String home() {
+		return msg;
+	}
+
+	@GetMapping("/hello-world")
+	public HelloWorldBean helloWorld() {
+		return new HelloWorldBean("Hello World");
+	}
+
+	@GetMapping("/hello-world/path-variabe/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World %s", name));
+	}
+
+	@GetMapping("/hello")
+	public String helloWorldInternationalization(
+			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.messages", null, locale);
+
+	}
+
 }
