720758fde158abd7c746da6b3b2c7a48629c2e52
Haimanti Saha <anik.saha.4096@gmail.com>
2018-07-03 21:16:09 -0400

    added delete method plus validation rules

--- src/main/java/com/practice/demo/controller/UserResourceController.java
+++ src/main/java/com/practice/demo/controller/UserResourceController.java
@@ -3,8 +3,11 @@ package com.practice.demo.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +39,7 @@ public class UserResourceController {
 	}
 	
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = userDaoService.save(user);
 		
 		URI location = ServletUriComponentsBuilder
@@ -47,5 +50,12 @@ public class UserResourceController {
 		return ResponseEntity.created(location).build();
 	}
 	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = userDaoService.deleteById(id);
+		if(user == null)
+			throw new UserNotFoundException("id -" + id);
+		
+	}
 
 }
--- src/main/java/com/practice/demo/exceptions/CustimizedResponseEntityExceptionHandler.java
+++ src/main/java/com/practice/demo/exceptions/CustimizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.practice.demo.exceptions;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +15,29 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 //@ControllerAdvice --> declare methods to be stored across multiple controller classes
 @ControllerAdvice
 @RestController
-public class CustimizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
-	
-	
+public class CustimizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request)  {
-		ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-	    return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
+
 	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<Object> handleUserNotFoundExceptions(Exception ex, WebRequest request)  {
-		ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-	    return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
+	public final ResponseEntity<Object> handleUserNotFoundExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Failed",
+				ex.getBindingResult().getAllErrors().toString());
+
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 
 }
